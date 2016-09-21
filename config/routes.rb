51139a4ce885d48f5b1ff7dfae9b71f91c668b23
John Robinson <john.wrobinson@hotmail.co.uk>
2016-09-21 18:35:18 +0100

    Added a route to config/routes.rb for the admin route in the home controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :players
   resources :game_types
   get 'home/index'
-
+  get 'home/admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
