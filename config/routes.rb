692d30423414cfad08a6553671860c3cef506306
John Robinson <john.wrobinson@hotmail.co.uk>
2016-09-21 22:38:46 +0100

    altered the access permissions for the controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,22 @@
 Rails.application.routes.draw do
   devise_for :admins
-  resources :player_in_leagues
-  resources :games
-  resources :leagues
-  resources :players
-  resources :game_types
+  resources :player_in_leagues, only: [:index, :show]
+  resources :games, only: [:index, :show]
+  resources :leagues, only: [:index, :show]
+  resources :players, only: [:index, :show]
+  resources :game_types, only: [:index, :show]
   get 'home/index'
   get 'home/admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'home#index'
+
+  authenticate :admin do
+    resources :player_in_leagues, only: [:new, :create, :edit, :update, :destroy]
+    resources :games, only: [:new, :create, :edit, :update, :destroy]
+    resources :leagues, only: [:new, :create, :edit, :update, :destroy]
+    resources :players, only: [:new, :create, :edit, :update, :destroy]
+    resources :game_types, only: [:new, :create, :edit, :update, :destroy]
+  end
+
 end
